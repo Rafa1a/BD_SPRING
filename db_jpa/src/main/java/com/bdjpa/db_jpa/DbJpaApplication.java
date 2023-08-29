13ec306809aa810e84631f69b1e1b3957ede0956
Rafael Leal Altero <rafa.altero@gmail.com>
2023-08-29 10:06:08 -0300

    up

---
+++ db_jpa/src/main/java/com/bdjpa/db_jpa/DbJpaApplication.java
@@ -0,0 +1,13 @@
+package com.bdjpa.db_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbJpaApplication.class, args);
+	}
+
+}
